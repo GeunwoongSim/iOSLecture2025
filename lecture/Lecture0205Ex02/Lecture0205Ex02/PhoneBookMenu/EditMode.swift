b6b923e2b8b75abebc02706c0bc1597a2baf40e0
심근웅 <geunwoong.sim@gmail.com>
2025-02-05 12:06:19 +0900

    remove Project

--- lecture/Lecture0205Ex02/Lecture0205Ex02/PhoneBookMenu/EditMode.swift
+++
@@ -1,5 +0,0 @@
-// 4)EDIT
-// 성명을 입력해서 전화번호 수정
-extension PhoneBook {
-    func editMode() { print("\n----- 수정 기능 -----") }
-}
--- lecture/Lecture0205Ex02/Lecture0205Ex02/PhoneBookMenu/SearchMode.swift
+++
@@ -1,6 +0,0 @@
-// 3)SEARCH
-// 이름으로 검색해서 전화번호 출력
-
-extension PhoneBook {
-    func searchMode() { print("\n----- 이름 검색 -----") }
-}
