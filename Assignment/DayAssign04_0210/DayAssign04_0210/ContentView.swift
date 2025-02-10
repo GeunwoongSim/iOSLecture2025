7a2b74ec11ca1dd28a66a481791384c0ffe7125b
심근웅 <geunwoong.sim@gmail.com>
2025-02-10 20:31:10 +0900

    Add Assign 04 Project

---
+++ Assignment/DayAssign04_0210/DayAssign04_0210/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayAssign04_0210
+//
+//  Created by 심근웅 on 2/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Assignment/DayAssign04_0210/DayAssign04_0210/DayAssign04_0210App.swift
@@ -0,0 +1,17 @@
+//
+//  DayAssign04_0210App.swift
+//  DayAssign04_0210
+//
+//  Created by 심근웅 on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DayAssign04_0210App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
